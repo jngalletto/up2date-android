dc93465e844a6389614027aafa7873164c77b632
Joaquin <jngalletto@gmail.com>
2018-11-22 23:56:35 -0300

    cover nullable attributes

--- app/src/main/kotlin/com/mooveit/genesis/model/post/Post.kt
+++ app/src/main/kotlin/com/mooveit/genesis/model/post/Post.kt
@@ -7,11 +7,11 @@ import kotlinx.android.parcel.Parcelize
 @Parcelize
 data class Post(
         val source: Source = Source(),
-        val author: String = "",
+        val author: String? = "",
         val title: String = "",
-        val description: String = "",
+        val description: String? = "",
         val url: String = "",
-        val urlToImage: String = "",
+        val urlToImage: String? = "",
         val publishedAt: String = "",
-        val content: String = ""
+        val content: String? = ""
 ) : Parcelable
