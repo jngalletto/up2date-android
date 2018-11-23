00e19db46f65e200535f1a33baa7e462c212335c
Joaquin <jngalletto@gmail.com>
2018-11-23 15:13:54 -0300

    add api key in client provider

--- app/src/main/kotlin/com/mooveit/genesis/service/PostService.kt
+++ app/src/main/kotlin/com/mooveit/genesis/service/PostService.kt
@@ -8,8 +8,6 @@ import retrofit2.http.Query
 
 interface PostService {
     companion object {
-        const val API_KEY = "apiKey"
-        const val API_KEY_VALUE = "17c9920762bd4f5f94241bfdd0008d19"
         const val COUNTRY_KEY = "country"
         const val DEFAULT_COUNTRY_CODE = "us"
 
@@ -18,7 +16,6 @@ interface PostService {
 
     @GET(HEADLINES_URL)
     fun getTopHeadLines(
-            @Query(COUNTRY_KEY) country: String = DEFAULT_COUNTRY_CODE,
-            @Query(API_KEY) apiKey: String = API_KEY_VALUE
+            @Query(COUNTRY_KEY) country: String = DEFAULT_COUNTRY_CODE
     ): Deferred<FetchPostsResponse>
 }
