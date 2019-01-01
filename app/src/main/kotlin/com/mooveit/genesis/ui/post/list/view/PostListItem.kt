df52dd452f7e547ebe1d545e004a5dfe4a1fb1b1
Joaquin <jngalletto@gmail.com>
2019-01-01 10:26:52 -0300

    add cardview for posts

--- app/src/main/kotlin/com/mooveit/genesis/ui/post/list/view/PostListItem.kt
+++ app/src/main/kotlin/com/mooveit/genesis/ui/post/list/view/PostListItem.kt
@@ -4,6 +4,7 @@ import android.view.View
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
+import com.mapright.android.helper.gone
 import com.mapright.android.helper.visibleIf
 import com.mooveit.genesis.model.post.Post
 import com.mooveit.genesis.ui.common.SingleItemViewHolder
@@ -17,10 +18,15 @@ class PostListItem(val view: View, val onClick: (Post) -> Unit) : SingleItemView
             title.text = item.title
             body.text = item.description
             body.visibleIf { item.description != null }
-            Glide.with(view)
-                .load(item.urlToImage ?: "https://static01.nyt.com/images/2018/11/27/us/27dc-manafort1/27dc-manafort1-superJumbo.jpg?quality=90&auto=webp")
-                .apply(RequestOptions.diskCacheStrategyOf(DiskCacheStrategy.ALL))
-                .into(newsImage)
+            item.urlToImage?.let {
+                Glide.with(view)
+                    .asBitmap()
+                    .load(item.urlToImage)
+                    .apply(RequestOptions.diskCacheStrategyOf(DiskCacheStrategy.ALL))
+                    .into(newsImage)
+            } ?: run {
+                newsImage.gone()
+            }
         }
     }
 }
