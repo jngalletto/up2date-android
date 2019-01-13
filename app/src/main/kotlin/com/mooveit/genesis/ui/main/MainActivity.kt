c86d9e5f57a39353191353d8d78e04b231553486
Joaquin <jngalletto@gmail.com>
2019-01-13 19:16:30 -0300

    add gms location services, add permissions required in manifest

--- app/src/main/kotlin/com/mooveit/genesis/ui/main/MainActivity.kt
+++ app/src/main/kotlin/com/mooveit/genesis/ui/main/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.mooveit.genesis.ui.main
 
+import android.location.Location
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.mooveit.genesis.router
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+
 
 class MainActivity : AppCompatActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
-    startActivity(router.routeToPostListActivity())
-    finish()
-  }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        startActivity(router.routeToPostListActivity())
+        finish()
+    }
 }
