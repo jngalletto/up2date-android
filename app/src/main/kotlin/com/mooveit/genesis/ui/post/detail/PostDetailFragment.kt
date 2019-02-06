3fc7a5b15a73ca2643ebfea6a5fd45d731304dd4
Joaquin <jngalletto@gmail.com>
2019-02-06 10:24:42 -0300

    add news image to post detail activity

--- app/src/main/kotlin/com/mooveit/genesis/ui/post/detail/PostDetailFragment.kt
+++ app/src/main/kotlin/com/mooveit/genesis/ui/post/detail/PostDetailFragment.kt
@@ -1,13 +1,11 @@
 package com.mooveit.genesis.ui.post.detail
 
 import android.support.v7.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.mapright.android.helper.gone
 import com.mooveit.genesis.R
-import com.mooveit.genesis.R.id.*
-import com.mooveit.genesis.helper.observe
-import com.mooveit.genesis.helper.performIfPresent
-import com.mooveit.genesis.repository.core.Resource
-import com.mooveit.genesis.repository.core.ResourceStatus
-import com.mooveit.genesis.ui.common.SingleItemAdapter
 import com.mooveit.genesis.ui.core.fragment.BaseNavigableFragment
 import kotlinx.android.synthetic.main.fragment_post_detail.*
 
@@ -29,6 +27,17 @@ class PostDetailFragment : BaseNavigableFragment<PostDetailViewModel>() {
     viewModel.post?.let {
       title.text = it.title
       body.text = it.description
+      activity?.let { context ->
+        it.urlToImage?.let {
+          Glide.with(context)
+              .asBitmap()
+              .load(it)
+              .apply(RequestOptions.diskCacheStrategyOf(DiskCacheStrategy.ALL))
+              .into(newsImage)
+        } ?: run {
+          newsImage.gone()
+        }
+      }
     }
   }
 }
